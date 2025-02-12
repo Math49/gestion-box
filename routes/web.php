441f40f8040381cd074585c3c229cae072ad87ce
Hockwood <mthsmercier@gmail.com>
2025-02-12 14:14:24 +0100

    Création des controllers

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LocataireController;
+use App\Http\Controllers\BoxController;
+use App\Http\Controllers\UserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +19,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+Route::middleware('auth')->group(function (){
+    Route::get('/locataires', [LocataireController::class, 'index'])->name('locataires.index');
 });
 
 require __DIR__.'/auth.php';
