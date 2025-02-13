c484de6b03d80694ab5282df337937cd3759b557
Hockwood <mthsmercier@gmail.com>
2025-02-13 09:33:43 +0100

    fixed #14 fixed #13 fixed #12 fixed #11

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UserController extends Controller
 {
-    //
+    public function getAllUsers()
+    {
+        $users = User::all();
+        return view('users', ['users' => $users]);
+    }
+
+    public function getUserById($id)
+    {
+        $user = User::find($id);
+        return view('user', ['user' => $user]);
+    }
 }
