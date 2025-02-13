f59c39c9f3b5d79d74f45e17feae8ed52f2753f2
Hockwood <mthsmercier@gmail.com>
2025-02-13 09:05:18 +0100

    fixed #23

--- app/Http/Controllers/LocataireController.php
+++ app/Http/Controllers/LocataireController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Exception;
+
+use App\Models\Locataire;
 
 class LocataireController extends Controller
 {
-    
+    public function createLocataire(Request $request)
+    {
+        try{
+
+            $request->validate([
+                'name' => 'required',
+                'prenom' => 'required',
+                'address' => 'required',
+                'telephone' => 'required',
+                'email' => 'required',
+                'bancaire' => 'required'
+            ]);
+
+            $user = $request->user();
+            
+            $locataire = new Locataire([
+                'Nom' => $request->input('name'),
+                'Prenom' => $request->input('prenom'),
+                'Adresse' => $request->input('address'),
+                'Telephone' => $request->input('telephone'),
+                'Email' => $request->input('email'),
+                'bancaire' => $request->input('bancaire'),
+                'ID_user' => $user->ID_user
+            ]);
+
+            $locataire->save();
+            return redirect()->route('dashboard');
+
+        }catch(Exception $e){
+            return redirect()->back()->with('error', 'An error occurred');
+        }
+    }
+
 }
